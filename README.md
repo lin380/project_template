0b272890eb2b138f5a30c4ea38ee521d043d26df
Jerid Francom <francojc@wfu.edu>
2021-07-20 00:45:19 -0400

    Initital commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# README
+
+This file is used to describe the project in general terms including the purpose of the project, data sources, analysis methods, and other relevant information to help clarify how to reproduce the research with these project files.
+
+If this file includes the `.md` extension, it can take full advantage of the Markdown syntax hooks to create a range of word processing-style formatting such as:
+
+# Headings
+
+## Sub-headings
+
+* Bullets 1
+* Bullets 2
+* Bullets 3
+
+**Bold text**, *italics*, ~~strikethrough~~, etc.
+
+Follow this link for [more on Markdown syntax options](https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf)
---
+++ _pipeline.R
@@ -0,0 +1,51 @@
+# ABOUT -------------------------------------------------------------------
+
+# Description: <The aim of this project>
+# Usage: <How to run this master script>
+# Author: <Your Name>
+# Date: <current date>
+
+# SETUP -------------------------------------------------------------------
+
+# Global project options or packages
+
+# Load `pacman`. If not installed, install then load.
+if (!require("pacman", character.only = TRUE)) {
+  install.packages("pacman")
+  library("pacman", character.only = TRUE)
+}
+
+pacman::p_load(tidyverse, rmarkdown, knitr, sessioninfo)
+
+# RUN ---------------------------------------------------------------------
+
+# 1. Acquire data
+
+render_site(input = "analysis/1_acquire_data.Rmd")
+
+# 2. Curate dataset
+
+render_site(input = "analysis/2_curate_dataset.Rmd")
+
+# 3. Transform dataset
+
+render_site(input = "analysis/3_transform_dataset.Rmd")
+
+# 4. Analyze dataset
+
+render_site(input = "analysis/4_analyze_dataset.Rmd")
+
+# 5. Generate reports
+
+render_site(input = "analysis/5_generate_article.Rmd")
+render(input = "analysis/5_generate_article.Rmd", output_format = "pdf_document", output_file = "article.pdf")
+
+# Create index
+
+render_site(input = "analysis/index.Rmd")
+
+# CLEAN UP ----------------------------------------------------------------
+
+# Remove all current environment variables
+rm(list = ls())
+
